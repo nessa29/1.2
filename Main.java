ddf3317b9a23190fe20e291270b20a26cf137907
nessa29 <vanessay10@nycstudents.net>
2024-09-11 16:22:52 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int i = 2;
+        i = i + 20;
+        final double n = 2.2;
+        String y = "hii";
+        boolean b = true;
+    System.out.print(i + n + y + b);
+    }
+    
+
+}
+
